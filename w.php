cbe30c39395ee44133f946af21093eb0fa9a3f43
quicurge <66072283+quicurge@users.noreply.github.com>
2020-05-28 20:12:59 +0700

    Add files via upload

---
+++ w.php
@@ -0,0 +1,42 @@
+
+ <!DOCTYPE html>
+<html>
+<head>
+        <title>Open in Instaram Camera din Față @cameradinfata !</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=600">
+        <link rel="stylesheet" href="/white/css/style.css">
+</head>
+    <body>
+        <div class="container">
+                 <h1>Camera din Față
+                    <a href="https://www.instagram.com/cameradinfata/">(@cameradinfata)</a>
+                    <img src="/white/cameradinfata.jpg" class="img-round" width="150">
+                 
+                </h1>
+				 <div style="margin-top: 30px;">
+                    <p class="open-descriptor">838 postări
+5.030 de urmăritori
+180 de urmăriri - See Instagram photos and videos from Camera din Față
+                        (@cameradinfata)</p>
+                </div>
+                <div style="margin-top: 30px;">
+                    <p class="">👵🏼 Instagramu' lu' Mamaie!
+🕰 De 8 ani alături de voi!
+🎬 #cameradinfata
+📞 Rezervări doar la 0213111512<br>
+                        </p>
+                </div>
+
+            <a href="https://www.instagram.com/cameradinfata/" class="btn btn-open">Open in Instagram</a>
+        
+        </div>
+
+        
+        <script>
+            setTimeout(function(){window.location.replace('https://www.instagram.com/cameradinfata/');}, 2500);
+        </script>
+    
+
+</body>
+</html>
